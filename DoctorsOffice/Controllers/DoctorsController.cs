ffeb972779972883d6efa77888d9a16b4e36928d
Student <example@example.com>
2019-05-14 14:36:59 -0700

    Add more views

--- DoctorsOffice/Controllers/DoctorsController.cs
+++ DoctorsOffice/Controllers/DoctorsController.cs
@@ -17,11 +17,47 @@ namespace DoctorsOffice.Controllers
           return View(allDoctors);
       }
 
+
       [HttpGet("/doctors/new")]
       public ActionResult New()
       {
         return View();
       }
+
+
+      [HttpPost("/doctors/create")]
+    public ActionResult Create(string doctorName, string doctorSpeciality)
+      {
+        Doctor newDoctor = new Doctor(doctorName, doctorSpeciality);
+        newDoctor.Save();
+        List<Doctor> allDoctors = Doctor.GetAll();
+        return View("Index", allDoctors);
+      }
+
+
+    [HttpGet("/doctors/{id}")]
+    public ActionResult Show(int id)
+      {
+          Dictionary<string, object> model = new Dictionary<string, object>();
+          Doctor selectedDoctor = Doctor.Find(id);
+          List<Patient> categoryPatients = selectedDoctor.GetPatients();
+          List<Patient> allPatients = Patient.GetAll();
+          model.Add("doctor", selectedDoctor);
+          model.Add("doctorPatients", categoryPatients);  
+          model.Add("allPatients", allPatients);
+          return View(model);
+      }
+
+
+    [HttpPost("/doctors/{doctorId}/patients/new")]
+    public ActionResult AddPatient(int doctorId, int patientId)
+      {
+        Doctor doctor = Doctor.Find(doctorId);
+        Patient patient = Patient.Find(patientId);
+        doctor.AddPatient(patient);
+        return RedirectToAction("Show",  new { id = doctorId });
+      }
+
 
   }
 }
--- DoctorsOffice/Controllers/PatientsController.cs
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -18,10 +18,45 @@ namespace DoctorsOffice.Controllers
       }
 
       [HttpGet("/patients/new")]
-      public ActionResult New()
+      public ActionResult New(int id)
       {
-        return View();
+          return View(id);
       }
+
+
+      [HttpPost("/patients")]
+      public ActionResult Create(string name, DateTime birthdate)
+      {
+        Patient newPatient = new Patient(name, birthdate);
+        newPatient.Save();
+        List<Patient> allPatients = Patient.GetAll();
+        return View("Index", allPatients);
+      }
+
+
+      [HttpGet("/patients/{id}")]
+      public ActionResult Show(int id)
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Patient selectedPatient = Patient.Find(id);
+        List<Doctor> patientDoctors = selectedPatient.GetDoctors();
+        List<Doctor> allDoctors = Doctor.GetAll();
+        model.Add("selectedPatient", selectedPatient);
+        model.Add("patientDoctors", patientDoctors);
+        model.Add("allDoctors", allDoctors);
+        return View(model);
+      }
+
+
+      [HttpPost("/patients/{patientId}/doctors/new")]
+      public ActionResult AddDoctor(int patientId, int doctorId)
+      {
+        Patient patient = Patient.Find(patientId);
+        Doctor doctor = Doctor.Find(doctorId);
+        patient.AddDoctor(doctor);
+        return RedirectToAction("Show",  new { id = patientId });
+      }
+
 
   }
 }
